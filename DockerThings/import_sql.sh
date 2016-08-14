5f1d5dce40451898701600f160621e8792738477
Justin F. Brunelle <jbrunelle@cs.odu.edu>
2016-08-13 20:02:12 -0400

    Add files via upload

---
+++ DockerThings/import_sql.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+RET=1
+while [[ RET -ne 0 ]]; do
+    echo "=> Waiting for MySQL to start"
+    sleep 5
+    mysql -uroot -e "status" > /dev/null 2>&1
+RET=$?
+done
+
+echo "=> MySQL Started"
+
+echo "=> Importing SQL file"
+mysql -uroot --force --verbose < /app/milestone4dump.sql
+
+echo "=> All done"
+
