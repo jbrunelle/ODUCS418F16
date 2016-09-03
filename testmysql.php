1234e2dd7f904a17e8e5596c2048e3e6595d1cf0
Justin F. Brunelle <jbrunelle@cs.odu.edu>
2016-09-03 09:33:50 -0400

    fixing test file

--- testmysql.php
+++ testmysql.php
@@ -23,11 +23,12 @@ if ($result->num_rows > 0) {
     echo "<tr><th>ID<th>opponent<th>visitor points<th>odu points<th>date\n";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<td>" . $row["id"] . "<td>" . $row["opponent"] . "<td>" . $row["visitorPoints"] . "<td>" . $row["oduPoints"] . "<td>" . $row["notes"] . "\n";
+        echo "<tr><td>" . $row["id"] . "<td>" . $row["opponent"] . "<td>" . $row["visitorPoints"] . "<td>" . $row["oduPoints"] . "<td>" . $row["notes"] . "\n";
     }
 } else {
     echo "0 results";
 }
+echo "</table>\n";
 $conn->close();
 echo "</body></html>";
 ?>
