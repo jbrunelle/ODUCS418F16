a92eaa0305eadae3cd045f36a0beaba00704ab39
Justin F. Brunelle <jbrunelle@cs.odu.edu>
2016-09-06 22:59:56 -0400

    Add files via upload

--- userinputs/unsafe.php
+++ userinputs/unsafe.php
@@ -7,7 +7,7 @@ $password = "";
 $dbname = "mysqltest";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysql($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
