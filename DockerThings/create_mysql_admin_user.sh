237975dadd2684bcbee7cf3699129de10b2cf1a5
Justin F. Brunelle <jbrunelle@cs.odu.edu>
2016-09-21 20:36:13 -0400

    Update create_mysql_admin_user.sh

--- DockerThings/create_mysql_admin_user.sh
+++ DockerThings/create_mysql_admin_user.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 /usr/bin/mysqld_safe > /dev/null 2>&1 &
 
 RET=1
@@ -10,9 +8,11 @@ while [[ RET -ne 0 ]]; do
     RET=$?
 done
 
-PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
+#PASS=${MYSQL_PASS:-$(pwgen -s 12 1)}
+PASS="M0n@rch$"
 _word=$( [ ${MYSQL_PASS} ] && echo "preset" || echo "random" )
-echo "=> Creating MySQL admin user with ${_word} password"
+#echo "=> Creating MySQL admin user with ${_word} password"
+echo "===>> Creating MySQL admin user with $PASS password"
 
 mysql -uroot -e "CREATE USER 'admin'@'%' IDENTIFIED BY '$PASS'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION"
@@ -23,7 +23,7 @@ echo "=> Done!"
 echo "========================================================================"
 echo "You can now connect to this MySQL Server using:"
 echo ""
-echo "    mysql -uadmin -p$PASS -h<host> -P<port>"
+echo "    mysql -u admin -p $PASS -h<host> -P<port>"
 echo ""
 echo "Please remember to change the above password as soon as possible!"
 echo "MySQL user 'root' has no password but only allows local connections"
